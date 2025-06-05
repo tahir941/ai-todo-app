99c1176fbaae0559ced918cad59e8d47bc946339
tahir941 <tahirabdulazeez10@gmail.com>
2025-06-05 20:00:11 +0100

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://ai-todo-app-ir47.onrender.com/api',
 });
 
 export const register = (userData) => API.post('/auth/register', userData);
