4af3e1a0b5e7b9a4bee639511ac058cadf5efb3a
tahir941 <tahirabdulazeez10@gmail.com>
2025-06-06 11:09:25 +0100

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+  baseURL: '/api', // ✅ relative path works because backend + frontend are together
 });
 
 export const register = (userData) => API.post('/auth/register', userData);
